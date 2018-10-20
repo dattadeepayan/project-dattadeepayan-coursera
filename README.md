2cdfe8aca6adaecd83c2096321f5f41497ff9d38
dattadeepayan <dattadeepayan@gmail.com>
2018-10-21 03:05:44 +0530

    Final Changes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 **Date and Time in UTC**
 
-Sat Oct 20 21:24:51 UTC 2018
+Sat Oct 20 21:35:23 UTC 2018
 
 **Lines Count in guessinggame.sh**
 
-22
+26
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,20 @@
+#!/usr/bin/env bash
+#Shell Scripting Project
 function match
 {	
 if [[ $1 -lt $2 ]]
 then
 	echo "Your guess is less. Guess Again"
+
 elif [[ $1 -gt $2 ]]
 then 
 	echo "YOur guess is high. Guess Again"
+
 fi
 }
 
 echo  "How many files are in the current directory?"
+
 read response
 var=$( ls | wc -l)
 while [[  $response -ne $var ]]
@@ -17,6 +22,5 @@ do
 	match $response $var
 	read response
 done
+
 echo "Congratulations" #this statement will get executed if only the code escapes from the above loop
-
-
